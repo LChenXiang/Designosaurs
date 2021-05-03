4e0020a57e51768c7a9262c2cd0f957d05d088ef
Lin Chen Xiang <clin0026@student.monash.edu>
2021-05-03 15:43:41 +0800

    Implemented BreedAction

--- src/game/actions/BreedAction.java
+++ src/game/actions/BreedAction.java
@@ -3,6 +3,9 @@ package game.actions;
 import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
+import game.behaviours.BreedBehaviour;
+import game.dinosaur.DinosaurStatus;
+import game.dinosaur.Gender;
 
 /**
  * Special Action breeds with an Actor
@@ -11,18 +14,44 @@ import edu.monash.fit2099.engine.GameMap;
  * @see Actor
  * @see Action
  * @see game.behaviours.BreedBehaviour
+ * @see DinosaurStatus
+ * @see Gender
  * @see GameMap
  * @since 03/05/2021
  */
 
 public class BreedAction extends Action {
+
+    /**
+     * Empty Constructor
+     */
+    public BreedAction() {}
+
+
+    /**
+     * executes breeding process, checks if actor is female, if so, make it pregnant, otherwise do nothing
+     * @param actor The actor performing the action.
+     * @param map The map the actor is on.
+     * @return null if actor is MALE, or a description that indicates actor is now pregnant
+     */
     @Override
     public String execute(Actor actor, GameMap map) {
+
+        if (actor.hasCapability(Gender.FEMALE)) {
+            actor.addCapability(DinosaurStatus.PREGNANT);
+            return actor + "is pregnant";
+        }
+
         return null;
     }
 
+    /**
+     * indicates that actor is mating on log
+     * @param actor The actor performing the action.
+     * @return description that actor is mating
+     */
     @Override
     public String menuDescription(Actor actor) {
-        return null;
+        return actor + "mates";
     }
 }

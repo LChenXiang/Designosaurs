0835479d0c93ef8c039c2b1bdd45beb84fc6b879
Lin Chen Xiang <clin0026@student.monash.edu>
2021-05-04 00:18:00 +0800

    Implemented LayEggAction

--- src/game/actions/LayEggAction.java
+++ src/game/actions/LayEggAction.java
@@ -3,6 +3,9 @@ package game.actions;
 import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
+import edu.monash.fit2099.engine.Location;
+import game.dinosaur.DinosaurStatus;
+import game.items.Egg;
 
 /**
  * Special Action that allows a Dinosaur to lay an Egg.
@@ -10,18 +13,37 @@ import edu.monash.fit2099.engine.GameMap;
  * @author Lin Chen Xiang
  * @see Actor
  * @see edu.monash.fit2099.engine.Location
- * @see game.items.EdibleItem
+ * @see Egg
  * @see game.dinosaur.DinosaurStatus
  * @see GameMap
  * @since 03/05/2021
  */
 
 public class LayEggAction extends Action {
+
+    /**
+     *
+     * @param actor The actor performing the action.
+     * @param map The map the actor is on.
+     * @return a description stating that the actor laid an egg
+     */
+
     @Override
     public String execute(Actor actor, GameMap map) {
 
-        return null;
+        Location here = map.locationOf(actor);
+        here.addItem(new Egg("Egg", 'O'));
+        // TODO: MAY NEED TO ADD ENUM OR SMTH TO INDICATE WHICH DINOSAUR
+
+        actor.removeCapability(DinosaurStatus.PREGNANT); // no longer pregnant
+        return actor + " lays an egg.";
     }
+
+    /**
+     *
+     * @param actor The actor performing the action.
+     * @return description about actor laying an egg
+     */
 
     @Override
     public String menuDescription(Actor actor) {

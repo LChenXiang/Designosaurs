433048b1b30ef0a1f7a3c16672303aaffbacea25
Lin Chen Xiang <clin0026@student.monash.edu>
2021-05-05 05:34:03 +0800

    documented some stuff on how to implement BuyItem

--- src/game/actions/BuyItem.java
+++ src/game/actions/BuyItem.java
@@ -5,13 +5,23 @@ import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
 
 public class BuyItem extends Action {
+
+    // init item
+    // init price
+
+    // construct(item, price)
+
     @Override
     public String execute(Actor actor, GameMap map) {
+        // add to inv (item)
+        // minus ecopoints (price)
+        // sysmsg player bought smth
         return null;
     }
 
     @Override
     public String menuDescription(Actor actor) {
+        // sysmenu player buys smth
         return null;
     }
 }

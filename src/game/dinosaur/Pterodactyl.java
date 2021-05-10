d03770d89a28151f4077ba8ec87625817427e5fb
Lin Chen Xiang <clin0026@student.monash.edu>
2021-05-10 14:44:39 +0800

    Added empty class Pterodactyl to be implemented later

---
+++ src/game/dinosaur/Pterodactyl.java
@@ -0,0 +1,90 @@
+package game.dinosaur;
+
+/**
+ * Represents a Carnivore Pterodactyl.
+ * Pterodactyls only eat Corpse and fishes in Lakes.
+ *
+ * @author Lin Chen Xiang
+ * @version 1.0
+ * @see DinosaurStatus
+ * @see Gender
+ * @see CarnivoreDinosaur
+ * @see Dinosaur
+ * @since 10/05/2021
+ */
+
+public class Pterodactyl extends CarnivoreDinosaur{
+
+    public Pterodactyl(Enum<Gender> gender) {
+        super("Pterodactyl", 'P', 100, gender);
+    }
+
+    public Pterodactyl() {
+        super("Pterodactyl", 'P', 100);
+    }
+
+    @Override
+    public int getCorpseRotTime() {
+        return 0;
+    }
+
+    @Override
+    public int getHungerThreshold() {
+        return 0;
+    }
+
+    @Override
+    public int getAdultAge() {
+        return 0;
+    }
+
+    @Override
+    public int getIncubationPeriod() {
+        return 0;
+    }
+
+    @Override
+    public int getStartingHP() {
+        return 0;
+    }
+
+    @Override
+    public int getBabyStartingHP() {
+        return 0;
+    }
+
+    @Override
+    public int getUnConsciousThreshold() {
+        return 0;
+    }
+
+    @Override
+    public int getPregnancyLength() {
+        return 0;
+    }
+
+    @Override
+    public int getWellFeedHunger() {
+        return 0;
+    }
+
+    @Override
+    public Dinosaur getNewDinosaur() {
+        return null;
+    }
+
+    @Override
+    public int getCorpseHealAmount() {
+        return 0;
+    }
+
+    @Override
+    public int getEggPurchasePrice() {
+        return 0;
+    }
+
+    @Override
+    public int getEggHatchEcoPoint() {
+        return 0;
+    }
+}

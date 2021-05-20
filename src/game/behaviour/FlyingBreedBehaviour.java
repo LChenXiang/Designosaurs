6e404a0db84d89500780bcc81cca8be6b9aea51d
Lin Chen Xiang <clin0026@student.monash.edu>
2021-05-20 17:21:06 +0800

    Added empty class FlyingBreedBehaviour

---
+++ src/game/behaviour/FlyingBreedBehaviour.java
@@ -0,0 +1,12 @@
+package game.behaviour;
+
+import edu.monash.fit2099.engine.Action;
+import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.GameMap;
+
+public class FlyingBreedBehaviour extends BreedBehaviour {
+    @Override
+    public Action getAction(Actor actor, GameMap map) {
+        return null;
+    }
+}
